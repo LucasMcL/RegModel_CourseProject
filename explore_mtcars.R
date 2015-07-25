16fb072ca273d5848efa6926873e9fed430df58e
Lucas McLaughlin <LMcLaughlin09@gmail.com>
2015-07-25 13:32:07 -0500

    Cleaned data; changed cyl, vs, and am to factor variables

--- explore_mtcars.R
+++ explore_mtcars.R
@@ -1,2 +1,14 @@
 ## This script includes code used in my exploratory analysis of the mtcars dataset
 
+library(ggplot2)
+library(dplyr)
+library(GGally)
+data(mtcars)
+
+mtcars <- mtcars %>% mutate(cyl = as.factor(as.character(cyl)), 
+                          vs = factor(vs, labels = c("V-engine", "Straight engine")), 
+                          am = factor(am, labels = c("Automatic", "Manual")))
+
+pairs(mtcars)
+ggpairs(mtcars[, 1:5], color = "cyl")
+cor(mtcars)[1, ]
