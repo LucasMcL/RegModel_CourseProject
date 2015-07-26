ddcde163275b856ecaed0426d97fe554bd9c08fd
Lucas McLaughlin <LMcLaughlin09@gmail.com>
2015-07-26 13:48:29 -0500

    Linear models fitted, coefficients described.  Just need to do some diagnostics and write conclusion.

--- explore_mtcars.R
+++ explore_mtcars.R
@@ -21,13 +21,13 @@ g + geom_boxplot() + xlab("Transmission") + ylab("MPG") +
 cor(old.mtcars)["am", ] # High cor with drat (rear axle ratio), wt, and gear
 cor(old.mtcars)["mpg", ] # High cor with cyl, disp, hp, drat, wt
 
-fit1 <- lm(mpg ~ am, data = mtcars) # Shows that manual cars, considering all else the same, have 7.245 better mpg
-# 95% CI that difference lies between summary(fit1)$coefficients[2, 1] +c(-1, 1) * summary(fit1)$coefficients[2, 2]
-fit2 <- update(fit1, mpg ~ am + disp)
-fit3 <- update(fit1, mpg ~ am + disp + hp)
-fit4 <- update(fit1, mpg ~ am + disp + hp + wt)
-fit5 <- update(fit1, mpg ~ am + disp + hp + wt + drat)
-anova(fit1, fit2, fit3, fit4, fit5)
+# fit1 <- lm(mpg ~ am, data = mtcars) # Shows that manual cars, considering all else the same, have 7.245 better mpg
+# # 95% CI that difference lies between summary(fit1)$coefficients[2, 1] +c(-1, 1) * summary(fit1)$coefficients[2, 2]
+# fit2 <- update(fit1, mpg ~ am + disp)
+# fit3 <- update(fit1, mpg ~ am + disp + hp)
+# fit4 <- update(fit1, mpg ~ am + disp + hp + wt)
+# fit5 <- update(fit1, mpg ~ am + disp + hp + wt + drat)
+# anova(fit1, fit2, fit3, fit4, fit5)
 
 # Graphs showing characteristics of Automatic and Manual cars
 p1 <- ggplot(mtcars, aes(x = am, y = wt)) + geom_boxplot() + ggtitle("Transmission and Weight")
@@ -35,3 +35,15 @@ p2 <- ggplot(mtcars, aes(x = am, y = drat)) + geom_boxplot() + ggtitle("Transmis
 p3 <- ggplot(mtcars, aes(x = am, y = disp)) + geom_boxplot() + ggtitle("Transmission and Engine Volume")
 p4 <- ggplot(mtcars, aes(x = am, y = gear)) + geom_boxplot() + ggtitle("Transmission and Number of Forward Gears")
 multiplot(p1, p2, p3, p4, cols = 2)
+
+g <- ggplot(mtcars, aes(x = wt, y = mpg, color = am))
+g + geom_point() + geom_smooth(method = "lm") + ggtitle("Car Weight and MPG") + 
+  xlab("Weight (1000s of lbs)") + ylab("MPG")
+
+fit1 <- lm(mpg ~ am, data = mtcars)
+fit2 <- lm(mpg ~ ., data = mtcars)
+fit3 <- lm(mpg ~ am + wt + am * wt, data = mtcars)
+summary(fit1)$coefficients
+summary(fit2)$coefficients
+summary(fit3)$coefficients
+
